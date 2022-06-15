f6e74d73e80aeaa57791eba6dffa14c9aa858a4f
PaulaMerlim <paula.merlim.77@gmail.com>
2022-06-14 22:54:06 -0300

    subindo o meu desafio aula3

---
+++ script.js
@@ -0,0 +1,28 @@
+// Exercício 1 -  Criar um loop que conte de 1 até 10 usando FOR
+
+for (let cont=1; cont<=10; cont++){ 
+   console.log (cont)
+}
+console.log("-------------------------------------------------")
+
+//2 - Criar um loop que conte de 10 até 1 usando WHILE
+
+let contador =10;
+while (contador >=1) {
+    console.log (contador)
+    contador --
+}
+console.log("-------------------------------------------------");
+
+//3 - Criar um loop que conte todos os números ímpares de 1 até 100
+
+
+for (let conteimpar =1; conteimpar <=100; conteimpar +=2) {
+    console.log (conteimpar);
+}
+console.log("-------------------------------------------------");
+
+//4- Criar um loop que conte todos os números pares de 0 a 100
+for (let contepar =0; contepar <=100; contepar +=2) {
+    console.log (contepar);
+}
